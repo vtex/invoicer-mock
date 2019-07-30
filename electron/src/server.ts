a3c7a10dcc9a129eea069119f14bafc7aadc49b4
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-07-30 11:59:12 -0300

    Working with vtex lint and automatic reload for server

---
+++ electron/src/server.ts
@@ -0,0 +1,63 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import * as core from 'express-serve-static-core'
+import * as http from 'http'
+
+let app: core.Express | null = null
+let appServer: http.Server
+const PORT = 6062
+
+function getNewApp() {
+  app = express()
+
+  app.use(bodyParser.json({ type: '*/json' }))
+
+  app.use(cors())
+
+  app.post('/', (req, res) => {
+    const msg = req.body
+    console.log(`Server message received: ${JSON.stringify(msg)}`, 'server')
+    res.json({ ...msg, extra: 'context' })
+  })
+
+  app.post('/invoice-order', (req, res) => {
+    const msg = req.body
+    console.log(`Server message received: ${JSON.stringify(msg)}`, 'server')
+    res.json(msg)
+  })
+
+  appServer = app.listen(PORT, () => {
+    console.log(`inStore server app listening on ${PORT}!`, 'server')
+  })
+
+  return app
+}
+
+function close() {
+  appServer.close()
+}
+
+function getApp() {
+  if (app) {
+    console.log(`Returning same app ${app}`)
+    return app
+  }
+
+  console.log(`Creating new app ${app}`)
+
+  return getNewApp()
+}
+
+function getDefaultEndpoint() {
+  return `http://localhost:${PORT}`
+}
+
+const server = {
+  close,
+  getApp,
+  getDefaultEndpoint,
+  getNewApp,
+}
+
+export default server
