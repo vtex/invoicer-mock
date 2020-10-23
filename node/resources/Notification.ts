5a46da4c9b4acfeaa6bb1b7343f53b3e9a3f7dbf
Rodrigo Imenes <rodrigoimenes@hotmail.com>
2020-10-23 09:59:30 -0300

    feat: add volumes and printable observation

--- node/resources/Notification.ts
+++ node/resources/Notification.ts
@@ -1,41 +1,48 @@
-import uuid from "uuid/v1";
-import ChangedItem from "./ChangedItem";
-import { InvoiceType } from "./Invoice";
+import uuid from 'uuid/v1'
+import ChangedItem from './ChangedItem'
+import { InvoiceType } from './Invoice'
+import { Volume } from './Volume'
 
 class Notification {
-  public id: string;
-  public items: ChangedItem[];
-  public changedItems: ChangedItem[];
-  public observation: string;
-  public timestamp: string;
-  public type: InvoiceType;
+  public id: string
+  public items: ChangedItem[]
+  public changedItems: ChangedItem[]
+  public observation: string
+  public timestamp: string
+  public type: InvoiceType
+  public volumes?: Volume[]
+  public printableObservation?: string
 
   public constructor(params: {
-    id: string;
-    items: ChangedItem[];
-    changedItems: ChangedItem[];
-    observation: string;
-    timestamp: string;
-    type: InvoiceType;
+    id: string
+    items: ChangedItem[]
+    changedItems: ChangedItem[]
+    observation: string
+    timestamp: string
+    type: InvoiceType
+    volumes?: Volume[]
+    printableObservation?: string
   }) {
-    this.id = params.id || uuid();
-    this.type = params.type || InvoiceType.Output;
+    this.id = params.id || uuid()
+    this.type = params.type || InvoiceType.Output
     this.changedItems =
       params.changedItems && params.changedItems.length > 0
-        ? params.changedItems.map(changedItem => new ChangedItem(changedItem))
-        : [];
+        ? params.changedItems.map((changedItem) => new ChangedItem(changedItem))
+        : []
     this.items =
       params.items && params.items.length > 0
-        ? params.items.map(changedItem => new ChangedItem(changedItem))
-        : [];
+        ? params.items.map((changedItem) => new ChangedItem(changedItem))
+        : []
 
-    this.observation = params.observation || "";
-    this.timestamp = new Date().toISOString();
+    this.observation = params.observation || ''
+    this.timestamp = new Date().toISOString()
+    this.volumes = params.volumes || [{ index: 0 }]
+    this.printableObservation = params.printableObservation || ''
   }
 
   public isValid() {
-    return true;
+    return true
   }
 }
 
-export default Notification;
+export default Notification
---
+++ node/resources/Volume.ts
@@ -0,0 +1,3 @@
+export type Volume = {
+  index: number
+}
